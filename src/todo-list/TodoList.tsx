137a823780794a9333aff10ef376d8b761227aa2
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2024-02-05 21:01:40 +0100

    refactor

--- src/todo-list/TodoList.tsx
+++ src/todo-list/TodoList.tsx
@@ -3,9 +3,13 @@ import TodoItem from './todo-item/TodoItem.tsx';
 
 function TodoList({todos}: { todos: string[] }) {
   
+  const listElements = todos.map(
+    (todo, index) => (<TodoItem key={index} index={index} todo={todo}></TodoItem>)
+  );
+  
   return (
     <div className="items">
-      {todos.map((todo, index) => (<TodoItem key={index} index={index} todo={todo}></TodoItem>))}
+      {listElements}
     </div>
   );
 }
