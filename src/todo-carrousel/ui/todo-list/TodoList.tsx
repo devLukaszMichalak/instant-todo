803718f3b8757bd6acc3f4e052bfeabeb5af0d34
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2024-03-22 23:26:16 +0100

    Update TodoList.tsx

--- src/todo-carrousel/ui/todo-list/TodoList.tsx
+++ src/todo-carrousel/ui/todo-list/TodoList.tsx
@@ -82,7 +82,7 @@ function TodoList({pageIndex}: Props) {
       </div>
       
       <div className="todo-list-buttons">
-        <button className="secondary" disabled={(pageIndex !== pageCount - 1) && pageCount !== 1} onClick={handleRemove}><MinusIcon style={IconStyle.dark}/></button>
+        <button className="secondary" disabled={pageCount === 1} onClick={handleRemove}><MinusIcon style={IconStyle.dark}/></button>
         <button className="secondary" onClick={handleClear}><ClearIcon style={IconStyle.dark}/></button>
         <button className="primary" onClick={handleNavigateToEdit}><EditIcon style={IconStyle.light}/></button>
         <button className="secondary" onClick={handleCopyShareLink}><ShareIcon style={IconStyle.dark}/></button>
